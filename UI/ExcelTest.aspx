e12e14b4d335628baf93d6f1fe8daa089a8c309f
Hilary <hilary.ramirez@ucrso.info>
2020-11-14 01:25:25 -0600

    Agregué los paquetes NuGet Excel Data Reader y la página de ejemplo ExcelTest, Comenté el navbar al Site Master

---
+++ UI/ExcelTest.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ExcelTest.aspx.cs" Inherits="WebApplication1.ExcelTest" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+<asp:FileUpload ID="FP" runat="server" />  
+     <asp:Button ID="Button1" runat="server" Text="Load Excel"  
+             OnClick="Button1_Click" />  
+     <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+    <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+
+
+</asp:Content>
---
+++ UI/ExcelTest.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.IO;
+using System.Data.OleDb;
+using System.Data;
+using ExcelDataReader;
+
+namespace WebApplication1
+{
+    public partial class ExcelTest : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            if (FP.PostedFile.ContentLength <= 0)
+            {
+                Label1.Text = "vacio";
+            }
+
+            //Get the file extension  
+            string fileExtension = Path.GetExtension(FP.FileName);
+
+            //If file is not in excel format then return  
+            if (fileExtension != ".xls" && fileExtension != ".xlsx")
+            { Label1.Text = "no es de excel"; }
+            else { 
+            
+            string path = Server.MapPath("~/excelstemp/" + FP.FileName);
+            //////saving the file inside the MyFolder of the server
+            FP.SaveAs(path);
+            using (var stream = File.Open(path, FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    var result = reader.AsDataSet();
+                    DataTable table = result.Tables[0];
+                    DataRow row = table.Rows[0];
+                    string cell = row[1].ToString();
+                    Label1.Text = cell;
+                    //DataTable table = result.Tables[0];
+                    //GridView1.DataSource = table;
+                    //GridView1.DataBind();
+                }
+            }
+            File.Delete(path);
+            }
+
+
+           
+
+           
+
+        }
+
+    }
+}
