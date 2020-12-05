82cc5257826169d334b934e39cf3e144cd9d8b58
Allan <allan.najera@gmail.com>
2020-12-05 10:30:00 -0600

    Creacion de paginas de Gestionar Facturas(CloseInvoice y PostponeInvoice)

---
+++ UI/PostponeInvoice.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PostponeInvoice.aspx.cs" Inherits="UI.PostponeInvoice" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+     <form id="form1" runat="server">
+        <div>
+            <div>
+               <h3>Aplazar Factura</h3>
+                <asp:Label ID="labelIdInvoice" runat="server" Text="Identificador de factura" CssClass="tittle"></asp:Label>
+                <br />
+                <asp:Label ID="lblIdInvoice" runat="server" Text="" CssClass="tittle"></asp:Label>
+                <br /><br />
+                <asp:Label ID="labelPaymentDate" runat="server" Text="Fecha de pago" CssClass="tittle"></asp:Label>
+                <br />
+                <asp:TextBox ID="txtPaymentDate" runat="server" TextMode="Date" CssClass="findtxt"></asp:TextBox>
+                <br /><br />
+                <asp:Button ID="btnSave" runat="server" Text="Guardar" OnClick="btnSave_Click" />
+                <asp:Button ID="btnCancel" runat="server" CausesValidation="False" Text="Cancelar" OnClick="btnCancel_Click" />
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ UI/PostponeInvoice.aspx.designer.cs
@@ -0,0 +1,80 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace UI
+{
+
+
+    public partial class PostponeInvoice
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control labelIdInvoice.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label labelIdInvoice;
+
+        /// <summary>
+        /// Control lblIdInvoice.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblIdInvoice;
+
+        /// <summary>
+        /// Control labelPaymentDate.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label labelPaymentDate;
+
+        /// <summary>
+        /// Control txtPaymentDate.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPaymentDate;
+
+        /// <summary>
+        /// Control btnSave.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnSave;
+
+        /// <summary>
+        /// Control btnCancel.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnCancel;
+    }
+}
