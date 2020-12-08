41e4bd0d3d544553fa0e0fe853ae494cd8aa5d1e
Hilary <hilary.ramirez@ucrso.info>
2020-12-07 21:54:39 -0600

    Cambios en el web.config y connString

--- DAO/Properties/Settings.Designer.cs
+++ DAO/Properties/Settings.Designer.cs
@@ -26,7 +26,8 @@ namespace DAO.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(local);Initial Catalog=Transtecno;Integrated Security=true")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=SQL5063.site4now.net;Initial Catalog=DB_A6AE64_Transtecno;User Id=DB_" +
+            "A6AE64_Transtecno_admin;Password=transtecno20")]
         public string connString {
             get {
                 return ((string)(this["connString"]));
